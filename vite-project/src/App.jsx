63abcab56a492e78a3ff087a25abcc8391f006f2
Kelsey Gazaway <Kelsey@Kelseys-MBP-7.attlocal.net>
2023-05-31 20:14:48 -0400

    second commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,47 @@
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-  
 
-  return (
-    <>
-      <div>
-    
+
+
+function App() {
+  const [featPupId, setFeatPupId] = useState(null)
+  const [puppies, setPuppies] = useState(puppyList)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
+
+ 
+    return (
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+            return (
+            <p class="pup" onClick = {() => { setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+              </p>
+             
+            )
+          })
+        } 
+
+        { featPupId && (
+        
+        <div id="featpup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+          
+        )}
+        
       </div>
 
-    </>
-  )
-}
+    )
+      }
+
+
 
 export default App
